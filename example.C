5116ce800c24142c6035734364e5582404e05234
Aviv Cukierman <avivcukierman@Avivs-MacBook-Pro.local>
2017-01-06 18:48:46 -0800

    First commit

---
+++ example.C
@@ -0,0 +1,171 @@
+#include "TRandom3.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TVector2.h"
+#include "TH2F.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include "TGenPhaseSpace.h"
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TColor.h"
+//#include "TRoot.h"
+#include "TMarker.h"
+#include "TH2F.h"
+
+void example(double number=10000, double pTmax = 0){
+
+    double mtop = 175.; // top quark mass in GeV
+    double mW = 80.;// W boson mass in GeV
+    double mB = 5.; // b quark mass in GeV.
+    double had = 0.0; //hadronization parameter
+    double loss = 0.0; //hadronization parameter
+    double swap = 0.0; //combinatoric noise
+
+    TH1F *mjjj = new TH1F ("","",40,0,400);
+
+    double pi = 4.*atan(1.);
+    TLorentzVector jet = TLorentzVector(0,0,0,mtop);
+    vector<double> masses;
+    masses.push_back(mW);
+    masses.push_back(mB);
+    TGenPhaseSpace event;
+    event.SetDecay(jet, masses.size(), &masses[0]); 
+    TGenPhaseSpace eventb;
+    eventb.SetDecay(jet, masses.size(), &masses[0]); 
+    vector<double> masses2;
+    masses2.push_back(0);
+    masses2.push_back(0);
+
+    TRandom3 rand = TRandom3(0);
+
+
+    TFile *out = new TFile("out.root","RECREATE");
+    std::vector<float> mjjj_vf;
+    TTree *tree = new TTree("oTree","");
+    float mjjj_f;
+    tree->Branch("mjjj",&mjjj_f);
+    float j1_pt; tree->Branch("j1_pt",&j1_pt);
+    float j1_eta; tree->Branch("j1_eta",&j1_eta);
+    float j1_phi; tree->Branch("j1_phi",&j1_phi);
+    float j1_m; tree->Branch("j1_m",&j1_m);
+    float j2_pt; tree->Branch("j2_pt",&j2_pt);
+    float j2_eta; tree->Branch("j2_eta",&j2_eta);
+    float j2_phi; tree->Branch("j2_phi",&j2_phi);
+    float j2_m; tree->Branch("j2_m",&j2_m);
+    float b_pt; tree->Branch("b_pt",&b_pt);
+    float b_eta; tree->Branch("b_eta",&b_eta);
+    float b_phi; tree->Branch("b_phi",&b_phi);
+    float b_m; tree->Branch("b_m",&b_m);
+    float W_pt; tree->Branch("W_pt",&W_pt);
+    float W_eta; tree->Branch("W_eta",&W_eta);
+    float W_phi; tree->Branch("W_phi",&W_phi);
+    float W_m; tree->Branch("W_m",&W_m);
+    float t_pt; tree->Branch("t_pt",&t_pt);
+    float t_eta; tree->Branch("t_eta",&t_eta);
+    float t_phi; tree->Branch("t_phi",&t_phi);
+    float t_m; tree->Branch("t_m",&t_m);
+
+    for (int i = 1; i<= number; i++){
+        if(!(i%1000)) std::cout << i << std::endl;
+
+        double phi = rand.Uniform(0.,2*pi);
+        double theta = rand.Uniform(0.,pi);
+        double sinTheta = sin(theta);
+        double cosTheta = cos(theta);
+           
+        double q=pTmax/(2*mtop*sin(theta));
+        double beta = q/sqrt(1+q*q);
+        TVector3 betavec = TVector3(beta*sinTheta*cos(phi), beta*sinTheta*sin(phi), beta*cosTheta);
+        event.Generate();
+        TLorentzVector *W1=event.GetDecay(0); 
+        TLorentzVector *b1=event.GetDecay(1); 
+        W1->Boost(betavec);
+        b1->Boost(betavec);
+        TLorentzVector top1 = *W1 + *b1;
+
+        int aaa = __LINE__; //for some reason it crashes if this isn't here
+
+        eventb.Generate();
+        TLorentzVector *W2=eventb.GetDecay(0); 
+        TLorentzVector *b2=eventb.GetDecay(1); 
+        W2->Boost(betavec);
+        b2->Boost(betavec);
+        TLorentzVector top2 = *W2 + *b2;
+
+        TGenPhaseSpace event2;
+        event2.SetDecay(*W1, masses2.size(), &masses2[0]); 
+        event2.Generate();
+        TLorentzVector *ele=event2.GetDecay(0); 
+        TLorentzVector *eleneut=event2.GetDecay(1); 
+
+        TGenPhaseSpace event2b;
+        event2b.SetDecay(*W2, masses2.size(), &masses2[0]); 
+        event2b.Generate();
+        TLorentzVector *j1=event2b.GetDecay(0); 
+        TLorentzVector *j2=event2b.GetDecay(1); 
+        //Parton shower, hadronization, jet clustering all in one.
+        j1->SetPtEtaPhiM(j1->Pt()*rand.Gaus(1-loss,had),j1->Eta(),j1->Phi(),0.);
+        j2->SetPtEtaPhiM(j2->Pt()*rand.Gaus(1-loss,had),j2->Eta(),j2->Phi(),0.);
+        b1->SetPtEtaPhiM(b1->Pt()*rand.Gaus(1-loss,had),b1->Eta(),b1->Phi(),mB);
+        b2->SetPtEtaPhiM(b2->Pt()*rand.Gaus(1-loss,had),b2->Eta(),b2->Phi(),mB);
+
+        TLorentzVector metT = TLorentzVector((*eleneut).Px(),(*eleneut).Py(),0.,(*eleneut).Pt());
+        TLorentzVector lepT = TLorentzVector(ele->Px(),ele->Py(),0.,ele->Pt()); 
+
+        if (rand.Uniform(0,1) > swap) mjjj_f = ((*j1)+(*j2)+(*b2)).M();
+        else mjjj_f = ((*j1)+(*j2)+(*b1)).M();
+
+        j1_pt = j1->Pt();
+        j1_eta = j1->Eta();
+        j1_phi = j1->Phi();
+        j1_m = j1->M();
+        j2_pt = j2->Pt();
+        j2_eta = j2->Eta();
+        j2_phi = j2->Phi();
+        j2_m = j2->M();
+        b_pt = b2->Pt();
+        b_eta = b2->Eta();
+        b_phi = b2->Phi();
+        b_m = b2->M();
+        W_pt = W2->Pt();
+        W_eta = W2->Eta();
+        W_phi = W2->Phi();
+        W_m = W2->M();
+        t_pt = top1.Pt();
+        t_eta = top1.Eta();
+        t_phi = top1.Phi();
+        t_m = top1.M();
+
+        tree->Fill();
+    }
+    out->Write();
+
+    /*
+    TCanvas *c1 = new TCanvas("","",750,750);
+    gStyle->SetOptStat(0);
+    gPad->SetLeftMargin(0.15);
+
+    mjjj->GetXaxis()->SetTitleOffset(1.2);
+    mjjj->GetXaxis()->SetTitle("m_{jjj} [GeV]");
+    mjjj->GetYaxis()->SetTitleOffset(1.8);
+    mjjj->GetYaxis()->SetNdivisions(505);
+    mjjj->GetYaxis()->SetTitle("Normalized to Unity");
+    mjjj->SetLineColor(2);
+
+    mjjj->Draw();
+
+    TLatex l  = TLatex();
+    l.SetNDC();
+    l.SetTextColor(1);
+    l.DrawLatex(0.15,0.92,"#bf{#scale[0.6]{Scalar Phase Space Only}}");
+    c1->Print("mjjj.pdf");*/
+
+}
+
+
---
+++ fit.py
@@ -0,0 +1,257 @@
+from numpy import array,mean,std,all,sqrt,asarray,average,save,histogram,cosh,append,savez,load,any,argsort,histogram2d,random,insert
+import os
+import numpy
+from optparse import OptionParser
+from sys import stdout,argv
+import ROOT as r
+import pdb
+#from helper_functions import *
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('text', usetex=True)
+#plt.style.use('atlas')
+import matplotlib.mlab as mlab
+
+parser = OptionParser()
+
+# job configuration
+parser.add_option("--inputDir", help="Directory containing input files",type=str, default=".")
+parser.add_option("--inputFile", help="Input file name",type=str, default="out.root")
+parser.add_option("--submitDir", help="Directory containing output files",type=str, default="output")
+parser.add_option("--plotDir", help="Directory containing plots",type=str, default="plots")
+parser.add_option("--numEvents", help="How many events to include (set to -1 for all events)",type=int, default=-1)
+#parser.add_option("--identifier", help="Identify dataset",type=str, default="VBFHiggs")
+parser.add_option("-r","--root", help="force reading in root files",action="store_true", default=False)
+
+# Root configuration
+parser.add_option("--j1_pt", help="jet 1 pT branch name",type=str, default="j1_pt")
+parser.add_option("--j1_eta", help="jet 1 eta branch name",type=str, default="j1_eta")
+parser.add_option("--j1_phi", help="jet 1 phi branch name",type=str, default="j1_phi")
+parser.add_option("--j1_m", help="jet 1 m branch name",type=str, default="j1_m")
+parser.add_option("--j2_pt", help="jet 2 pT branch name",type=str, default="j2_pt")
+parser.add_option("--j2_eta", help="jet 2 eta branch name",type=str, default="j2_eta")
+parser.add_option("--j2_phi", help="jet 2 phi branch name",type=str, default="j2_phi")
+parser.add_option("--j2_m", help="jet 2 m branch name",type=str, default="j2_m")
+parser.add_option("--b_pt", help="jet 1 pT branch name",type=str, default="b_pt")
+parser.add_option("--b_eta", help="jet 1 eta branch name",type=str, default="b_eta")
+parser.add_option("--b_phi", help="jet 1 phi branch name",type=str, default="b_phi")
+parser.add_option("--b_m", help="jet 1 m branch name",type=str, default="b_m")
+
+(options, args) = parser.parse_args()
+
+if not os.path.exists(options.inputDir): raise OSError(options.inputDir +' does not exist. This is where the input Root files go.')
+if not os.path.exists(options.submitDir):
+  print '== Making folder '+options.submitDir+' =='
+  os.makedirs(options.submitDir)
+if not os.path.exists(options.plotDir):
+  print '== Making folder '+options.plotDir+' =='
+  os.makedirs(options.plotDir)
+
+def read():
+  import glob
+  global cutflow
+
+  if len(options.inputFile)>0:
+    filenames = glob.glob(options.inputDir+'/'+options.inputFile)
+    if len(filenames) == 0: raise OSError('Can\'t find file '+options.inputDir+'/'+options.inputFile) 
+  else:
+    filenames = glob.glob(options.inputDir+'/*.root')
+    if len(filenames) == 0: raise OSError('Can\'t find files in '+options.inputDir) 
+  tree = r.TChain('oTree')
+  for filename in filenames:
+    statinfo = os.stat(filename)
+    if statinfo.st_size < 10000: continue #sometimes batch jobs fail
+    print '== Reading in '+filename+' =='
+
+    tree.Add(filename) 
+
+  # make sure the branches are compatible between the two
+  branches = set(i.GetName() for i in tree.GetListOfBranches())
+
+  # required:
+  if options.j1_pt not in branches: raise RuntimeError(options.j1_pt+' branch does not exist. This is the branch containing jet 1 pTs.')
+  else: print '== \''+options.j1_pt+'\' branch is being read as jet 1 pTs =='
+  if options.j1_eta not in branches: raise RuntimeError(options.j1_eta+' branch does not exist. This is the branch containing jet 1 etas.')
+  else: print '== \''+options.j1_eta+'\' branch is being read as jet 1 etas =='
+  if options.j1_phi not in branches: raise RuntimeError(options.j1_phi+' branch does not exist. This is the branch containing jet 1 phis.')
+  else: print '== \''+options.j1_phi+'\' branch is being read as jet 1 phis =='
+  if options.j1_m not in branches: raise RuntimeError(options.j1_m+' branch does not exist. This is the branch containing jet 1 ms.')
+  else: print '== \''+options.j1_m+'\' branch is being read as jet 1 ms =='
+  if options.j2_pt not in branches: raise RuntimeError(options.j2_pt+' branch does not exist. This is the branch containing jet 2 pTs.')
+  else: print '== \''+options.j2_pt+'\' branch is being read as jet 2 pTs =='
+  if options.j2_eta not in branches: raise RuntimeError(options.j2_eta+' branch does not exist. This is the branch containing jet 2 etas.')
+  else: print '== \''+options.j2_eta+'\' branch is being read as jet 2 etas =='
+  if options.j2_phi not in branches: raise RuntimeError(options.j2_phi+' branch does not exist. This is the branch containing jet 2 phis.')
+  else: print '== \''+options.j2_phi+'\' branch is being read as jet 2 phis =='
+  if options.j2_m not in branches: raise RuntimeError(options.j2_m+' branch does not exist. This is the branch containing jet 2 ms.')
+  else: print '== \''+options.j2_m+'\' branch is being read as jet 2 ms =='
+  if options.b_pt not in branches: raise RuntimeError(options.b_pt+' branch does not exist. This is the branch containing b jet pTs.')
+  else: print '== \''+options.b_pt+'\' branch is being read as b jet pTs =='
+  if options.b_eta not in branches: raise RuntimeError(options.b_eta+' branch does not exist. This is the branch containing b jet etas.')
+  else: print '== \''+options.b_eta+'\' branch is being read as b jet etas =='
+  if options.b_phi not in branches: raise RuntimeError(options.b_phi+' branch does not exist. This is the branch containing b jet phis.')
+  else: print '== \''+options.b_phi+'\' branch is being read as b jet phis =='
+  if options.b_m not in branches: raise RuntimeError(options.b_m+' branch does not exist. This is the branch containing b jet ms.')
+  else: print '== \''+options.b_m+'\' branch is being read as b jet ms =='
+
+  nentries = tree.GetEntries()
+
+  t_jet1s = []
+  t_jet2s = []
+  t_bjets = []
+  t_Ws = []
+  t_ts = []
+
+  for jentry in xrange(nentries):
+    if jentry>options.numEvents and options.numEvents>0: continue
+    tree.GetEntry(jentry)
+
+    if not jentry%1000:
+      stdout.write('== \r%d events read ==\n'%jentry)
+      stdout.flush() 
+
+    #Ntruthphotons = getattr(tree,options.Ntruthphotons)
+    #if not Ntruthphotons == 2: continue #only events with 2 truth photons
+
+    jet1 = r.TLorentzVector()
+    jet1.SetPtEtaPhiM(getattr(tree,options.j1_pt),getattr(tree,options.j1_eta),getattr(tree,options.j1_phi),getattr(tree,options.j1_m))
+    t_jet1s.append(jet1)
+
+    jet2 = r.TLorentzVector()
+    jet2.SetPtEtaPhiM(getattr(tree,options.j2_pt),getattr(tree,options.j2_eta),getattr(tree,options.j2_phi),getattr(tree,options.j2_m))
+    t_jet2s.append(jet2)
+
+    bjet = r.TLorentzVector()
+    bjet.SetPtEtaPhiM(getattr(tree,options.b_pt),getattr(tree,options.b_eta),getattr(tree,options.b_phi),getattr(tree,options.b_m))
+    t_bjets.append(bjet)
+
+    W = jet1+jet2
+    t_Ws.append(W)
+    t_ts.append(W+bjet)
+
+  return t_jet1s,t_jet2s,t_bjets,t_Ws,t_ts
+
+def smear(jets):
+  smeared_jets = []
+  smears = random.normal(0,7,len(jets)) #smear jets by 7 GeV
+  for j,s in zip(jets,smears):
+    sj = r.TLorentzVector()
+    sj.SetPtEtaPhiM(j.Pt()+s,j.Eta(),j.Phi(),j.M())
+    smeared_jets.append(sj)
+  return array(smeared_jets)
+
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+def learn(jets,tjets):
+  X = array([j.Pt() for j in jets])
+  Y = array([tj.Pt() for tj in tjets])
+  model = Sequential()
+  model.add(Dense(5, input_dim=1, init='uniform', activation='sigmoid'))
+  #model.add(Dense(8, init='uniform', activation='relu'))
+  model.add(Dense(1, init='uniform')) # regression
+  model.compile(loss='mean_squared_error', optimizer='adam', metrics=[])
+  model.fit(X, Y, nb_epoch=30, batch_size=10)
+  L = model.predict(X)
+  L = array([L[i][0] for i in range(len(L))]) #reshape
+
+  learned_jets = []
+  for lpt in L:
+    lj = r.TLorentzVector()
+    lj.SetPtEtaPhiM(lpt,j.Eta(),j.Phi(),j.M())
+    learned_jets.append(lj)
+  return array(learned_jets)
+
+def plot(objects,label='',name=''):
+  data = [o.Pt() for o in objects]
+  binwidth = 2
+  n,bins = numpy.histogram(data,normed=True,bins=numpy.arange(0, 100, binwidth))
+  n = insert(n,0,0)
+  n = n/sum(n)
+  plt.plot(bins,n,drawstyle='steps',ls='-',color='b')#label=label
+  plt.ylim(0,max(n)*1.1)
+  plt.xlim(0,100)
+  plt.xlabel(label+r' $p_T$')
+  plt.ylabel('a.u.')
+  plotname = name+'_pt'
+  plt.savefig(options.plotDir+'/'+plotname+'.png')
+  plt.close()
+
+  data = [o.Eta() for o in objects]
+  binwidth = 0.1
+  n,bins = numpy.histogram(data,normed=True,bins=numpy.arange(-3, 3, binwidth))
+  n = insert(n,0,0)
+  n = n/sum(n)
+  plt.plot(bins,n,drawstyle='steps',ls='-',color='b')#label=label
+  plt.ylim(0,max(n)*1.1)
+  plt.xlim(-3,3)
+  plt.xlabel(label+r' $\eta$')
+  plt.ylabel('a.u.')
+  plotname = name+'_eta'
+  plt.savefig(options.plotDir+'/'+plotname+'.png')
+  plt.close()
+
+  data = [o.Phi() for o in objects]
+  binwidth = 0.1
+  n,bins = numpy.histogram(data,normed=True,bins=numpy.arange(-3.2, 3.2, binwidth))
+  n = insert(n,0,0)
+  n = n/sum(n)
+  plt.plot(bins,n,drawstyle='steps',ls='-',color='b')#label=label
+  plt.ylim(0,max(n)*1.1)
+  plt.xlim(-3.2,3.2)
+  plt.xlabel(label+r' $\phi$')
+  plt.ylabel('a.u.')
+  plotname = name+'_phi'
+  plt.savefig(options.plotDir+'/'+plotname+'.png')
+  plt.close()
+
+  data = [o.M() for o in objects]
+  binwidth = 5 
+  n,bins = numpy.histogram(data,normed=True,bins=numpy.arange(0, 250, binwidth))
+  n = insert(n,0,0)
+  n = n/sum(n)
+  plt.plot(bins,n,drawstyle='steps',ls='-',color='b')#label=label
+  plt.ylim(0,max(n)*1.1)
+  plt.xlim(0,250)
+  plt.xlabel(label+r' $M$')
+  plt.ylabel('a.u.')
+  plotname = name+'_m'
+  plt.savefig(options.plotDir+'/'+plotname+'.png')
+  plt.close()
+
+
+
+t_jet1s,t_jet2s,t_bjets,t_Ws,t_ts = read()
+
+jet1s = smear(t_jet1s)
+jet2s = smear(t_jet2s)
+bjets = smear(t_bjets)
+Ws = jet1s+jet2s
+ts = Ws+bjets
+
+l_jet1s = learn(jet1s,t_jet1s)
+l_jet2s = learn(jet2s,t_jet2s)
+l_bjets = learn(bjets,t_bjets)
+l_Ws = l_jet1s+l_jet2s
+l_ts = l_Ws+l_bjets
+
+plot(t_jet1s,label='Jet 1',name='tjet1')
+plot(t_jet2s,label='Jet 2',name='tjet2')
+plot(t_bjets,label='b-Jet',name='tbjet')
+plot(t_Ws,label='W',name='tW')
+plot(t_ts,label='Top',name='tt')
+
+plot(jet1s,label='Jet 1',name='jet1')
+plot(jet2s,label='Jet 2',name='jet2')
+plot(bjets,label='b-Jet',name='bjet')
+plot(Ws,label='W',name='W')
+plot(ts,label='Top',name='t')
+
+plot(l_jet1s,label='Jet 1',name='ljet1')
+plot(l_jet2s,label='Jet 2',name='ljet2')
+plot(l_bjets,label='b-Jet',name='lbjet')
+plot(l_Ws,label='W',name='lW')
+plot(l_ts,label='Top',name='lt')
